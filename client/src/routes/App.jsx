b72f1341752fa111dcb497753c47763b86616cd7
Ztrebo14 <robertabon0414@gmail.com>
2024-05-11 16:56:24 +0800

    first commit

---
+++ client/src/routes/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState()
+
+  return (
+    <>
+    <h3>Starting of App</h3>
+    </>
+  )
+}
+
+export default App
